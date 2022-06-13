f128970fc81e9271a0c386439d76b73a4d494530
kubaslawski <kubaslawski.it@gmail.com>
2022-06-13 11:25:26 +0200

    14. Product & User interfaces updated.

--- src/interfaces/products.ts
+++ src/interfaces/products.ts
@@ -1,10 +1,19 @@
+import {ISeller} from "./users";
+import {IOption} from "./global";
+
+export interface IProductRating {
+    avg_rate: number;
+    rate_count: number;
+}
+
 export interface IProduct {
     id: number;
-    seller: number;
-    category: number;
+    seller: ISeller;
+    category: IOption;
     name: string;
     description: string;
     stock: number;
     price: number;
     photo: string;
+    product_rating: IProductRating;
 }
---
+++ src/interfaces/users.ts
@@ -0,0 +1,6 @@
+export interface ISeller {
+    id: number;
+    email: string;
+    first_name: string;
+    last_Name: string;
+}
