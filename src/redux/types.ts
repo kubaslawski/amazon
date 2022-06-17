5a4768d6c319eaa42a255201b1183b8570ac6b14
kubaslawski <kubaslawski.it@gmail.com>
2022-06-17 11:50:02 +0200

    26. Creating User Reducer.

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -8,6 +8,12 @@ export const GET_PRODUCT = "GET_PRODUCT";
 export const ADD_PRODUCT = "ADD_PRODUCT";
 
 // users
+export const SET_USER = "SET_USER";
+export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
+export const SET_UNAUTHENTICATED = "SET_UNAUTHENTICATED";
 
-// global
+// ui
 export const SET_ERRORS = "SET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const LOADING_UI = "LOADING_UI";
+export const STOP_LOADING_UI = "STOP_LOADING_UI";
