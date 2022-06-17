57a5f2fbe8373cda1cda354ddbf56c97152bceae
kubaslawski <kubaslawski.it@gmail.com>
2022-06-17 11:44:05 +0200

    25. UI Reducer.

---
+++ src/redux/reducers/ui.ts
@@ -0,0 +1,46 @@
+import {
+    SET_ERRORS,
+    CLEAR_ERRORS,
+    LOADING_UI,
+    STOP_LOADING_UI
+} from "../types";
+import {IAction} from "../../interfaces/global";
+
+interface IUIInitialState {
+    loading: boolean;
+    errors: unknown;
+}
+
+const initialState: IUIInitialState = {
+    loading: false,
+    errors: null
+}
+
+export default function(state: IUIInitialState = initialState, action: IAction){
+    switch(action.type){
+        case SET_ERRORS:
+            return {
+                ...state,
+                loading: false,
+                errors: action.payload
+            };
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                loading: false,
+                errors: null
+            }
+        case LOADING_UI:
+            return {
+                ...state,
+                loading: true
+            }
+        case STOP_LOADING_UI:
+            return {
+                ...state,
+                loading: false
+            }
+        default:
+            return state;
+    }
+}
