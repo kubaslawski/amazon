efac913dd57bc7a8ceaf09d7aaec634966456f90
kubaslawski <kubaslawski.it@gmail.com>
2022-07-08 14:18:38 +0200

    69. Dispatching categories info from menu component.

--- src/app/components/left-menu/left-menu.tsx
+++ src/app/components/left-menu/left-menu.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './left-menu.scss';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+// actions
+import {getAllCategories} from "../../../redux/actions/categories";
 // components
 import CategoriesMenuSection from "./sections/categories/categories";
 // interfaces
 import {ICategory} from "../../../interfaces/categories";
 import {IState} from "../../../redux/store";
+import {IDispatchInterface} from "../../../interfaces/global";
 // icons/photos
 import user from "../../images/user.png";
 
 
 
+
+
 const LeftMenu: React.FC = () => {
 
+    const dispatch: IDispatchInterface = useDispatch();
     const categories: Array<ICategory> = useSelector((state: IState) => state.categories.categories);
+
+    useEffect(() => {
+        if(categories === []){
+            dispatch(getAllCategories());
+        }
+    }, [dispatch])
 
     return (
         <div className={'left-menu'}>
