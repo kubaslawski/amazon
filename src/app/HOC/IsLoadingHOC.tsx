653e09794c92d310ab6c5af07b1484cc34beed3a
kubaslawski <kubaslawski.it@gmail.com>
2022-07-11 14:59:58 +0200

    73. Loading HOC rework.

--- src/app/HOC/IsLoadingHOC.tsx
+++ src/app/HOC/IsLoadingHOC.tsx
@@ -2,22 +2,23 @@ import React, {useState} from "react";
 import './isLoadingHOC.scss';
 import LoadingIcons from 'react-loading-icons'
 
-export const IsLoadingHOC = (WrappedComponent: React.FunctionComponent) => {
-    function HOC(props: any) {
-        const [isLoading, setIsLoading] = useState(true);
-
-        const setLoadingState = (isComponentLoading: boolean) => {
-            setIsLoading(isComponentLoading);
-        };
-
-        return (
-            <>
-                {isLoading && <LoadingIcons.TailSpin className={'hoc-loader-icon'} stroke={'#ff9900'}/>}
-                <WrappedComponent {...props} setLoading={setLoadingState}/>
-            </>
-        );
-    }
-    return HOC;
+export interface IBaseLoadableComponent {
+    setLoading: (isLoading: boolean) => void;
 }
 
-export default IsLoadingHOC;
+export const isLoadingHOC = (WrappedComponent: React.FunctionComponent<IBaseLoadableComponent>) => (props: any) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const setLoadingState = (isComponentLoading: boolean) => {
+        setIsLoading(isComponentLoading);
+    };
+
+    return (
+        <>
+            {isLoading && <LoadingIcons.TailSpin className={'hoc-loader-icon'} stroke={'#ff9900'}/>}
+            <WrappedComponent {...props} setLoading={setLoadingState}/>
+        </>
+    );
+}
+
+export default isLoadingHOC;
